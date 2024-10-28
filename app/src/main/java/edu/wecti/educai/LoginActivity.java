14b9bffc19249c1ad25b62be5f2d71acc3ad52bc
gustavomotamacedo <mota.macedo05@gmail.com>
2024-10-28 20:48:14 -0300

    XML tela inicial

--- app/src/main/java/edu/wecti/educai/LoginActivity.java
+++ app/src/main/java/edu/wecti/educai/LoginActivity.java
@@ -1,8 +1,6 @@
 package edu.wecti.educai;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,32 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button btnLogin, btnCadastro;
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        btnLogin = findViewById(R.id.btnLogin);
-        btnCadastro = findViewById(R.id.btnCadastro);
-
-        btnLogin.setOnClickListener(v -> {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        });
-
-        btnCadastro.setOnClickListener(v -> {
-            Intent intent = new Intent(this, CadastroActivity.class);
-            startActivity(intent);
         });
     }
 }
