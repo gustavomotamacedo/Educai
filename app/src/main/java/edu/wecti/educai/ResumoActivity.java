92a14b421900b4ccb3c23b839edf3794dc43532b
gustavomotamacedo <mota.macedo05@gmail.com>
2024-10-30 19:17:41 -0300

    Tela de resumo mockada + navegação

--- app/src/main/java/edu/wecti/educai/ResumoActivity.java
+++ app/src/main/java/edu/wecti/educai/ResumoActivity.java
@@ -1,36 +1,24 @@
 package edu.wecti.educai;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.utils.widget.MockView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class RoadmapActivity extends AppCompatActivity {
-
-    private MockView mockClickableTest;
+public class ResumoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_roadmap);
+        setContentView(R.layout.activity_resumo);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        mockClickableTest = findViewById(R.id.mockView);
-
-        mockClickableTest.setOnClickListener(v -> {
-            Intent intent = new Intent(this, ResumoActivity.class);
-            startActivity(intent);
         });
     }
 }
