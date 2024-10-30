a0daba14ea7a7b11c5ded62aa70995e61616dbb9
gustavomotamacedo <mota.macedo05@gmail.com>
2024-10-30 18:58:04 -0300

    Tela de roadmap + navegação

--- app/src/main/java/edu/wecti/educai/RoadmapActivity.java
+++ app/src/main/java/edu/wecti/educai/RoadmapActivity.java
@@ -9,30 +9,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.RecyclerView;
 
-import org.w3c.dom.Text;
-
-public class ConteudosActivity extends AppCompatActivity {
-
-    private TextView txtNome;
+public class RoadmapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_conteudos);
+        setContentView(R.layout.activity_roadmap);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        txtNome = findViewById(R.id.txtNome);
-
-        txtNome.setOnClickListener(v -> {
-            Intent intent = new Intent(this, RoadmapActivity.class);
-            startActivity(intent);
         });
     }
 }
