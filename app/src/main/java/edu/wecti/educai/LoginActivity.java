2b9817d3264370acd4ef8ba3a45c1b723eba632a
gustavomotamacedo <mota.macedo05@gmail.com>
2024-10-30 18:04:53 -0300

    Navegação entre login e menu

--- app/src/main/java/edu/wecti/educai/LoginActivity.java
+++ app/src/main/java/edu/wecti/educai/LoginActivity.java
@@ -1,6 +1,8 @@
 package edu.wecti.educai;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,14 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MenuActivity.class);
+            finish();
+            startActivity(intent);
         });
     }
 }
